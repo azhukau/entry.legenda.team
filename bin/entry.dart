c4c5589213f7fb824d801f835f0a44352964217e
Aliaksei Zhukau <zhukau.aliaksei@gmail.com>
2018-05-18 22:24:18 +0300

    Server start-up

---
+++ bin/entry.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'dart:io';
+
+String certificateChain = '/etc/letsencrypt/live/entry.legenda.team/fullchain.pem';
+String serverKey = '/etc/letsencrypt/live/entry.legenda.team/privkey.pem';
+
+Future main() async {
+  var serverContext = new SecurityContext();
+  serverContext.useCertificateChain(certificateChain);
+  serverContext.usePrivateKey(serverKey);
+
+  var server = await HttpServer.bindSecure(
+    'localhost',
+    4047,
+    serverContext,
+  );
+  print('Listening on localhost:${server.port}');
+  await for (HttpRequest request in server) {
+    request.response
+      ..write('Hello, world!')
+      ..close();
+  }
+}
