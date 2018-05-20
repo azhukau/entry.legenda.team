a2aef13eb06c167286627e6646b949ec1d2feff2
Aliaksei Zhukau <zhukau.aliaksei@gmail.com>
2018-05-20 18:07:06 +0300

    Prepare new row for the users table

--- bin/entry.dart
+++ bin/entry.dart
@@ -14,10 +14,31 @@ Future main() async {
     4047,
     serverContext,
   );
-  print('Listening on localhost:${server.port}');
-  await for (HttpRequest request in server) {
-    request.response
-      ..write('Hello, world!')
+  print("Listening on localhost:${server.port}");
+  await for (HttpRequest req in server) {
+
+    final HttpResponse resp = req.response;
+
+    final String last_name = req.uri.queryParameters['last'];
+    final String first_name = req.uri.queryParameters['first'];
+    final String year = req.uri.queryParameters['year'];
+    final String team = req.uri.queryParameters['team'];
+    final String si = req.uri.queryParameters['si'];
+    final String course = req.uri.queryParameters['course'].substring(0,3);
+
+    final String r = """
+            <tr>
+              <td>$last_name</td>
+              <td>$first_name</td>
+              <td>$year</td>
+              <td>$team</td>
+              <td>$si</td>
+              <td>$course</td>
+              <td></td>
+            </tr>
+        """;
+      resp
+      ..write(r)
       ..close();
   }
 }
